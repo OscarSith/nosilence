43205324730edd0790aac3c77e5af407e94a21bd
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2015-08-05 10:01:13 -0500

    Nuevo titulo de 15 años, agregado mas iconos en Servicios y corregido el orden de los mismos

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -17,7 +17,7 @@
 		                <span class="header">Catering</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-03.jpg">
+		        <li>
 		            <img src="images/portfolio/3-Fuente de Chocolate.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Fuente de Chocolate</span>
@@ -41,67 +41,67 @@
 		                <span class="header">Decoración y Arquitectura</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-07.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-27.jpg">
 		            <img src="images/portfolio/7-Invitaciones.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Invitaciones</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-08.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-09.jpg">
 		            <img src="images/portfolio/8-Control de Ingreso y Seguridad.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Control de Ingreso</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-09.jpg">
+		        <li>
 		            <img src="images/portfolio/9-Sonido Profesional.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Sonico Profesional</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-10.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-11.jpg">
 		            <img src="images/portfolio/10-Deejay Session.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Deejay Session</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-11.jpg">
+		        <li>
 		            <img src="images/portfolio/11-Iluminacion y Efectos.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Iluminación y Efectos</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-12.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-15.jpg">
 		            <img src="images/portfolio/12-Animacion y Entrevistas.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Animación y Entrevistas</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-13.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-10.jpg">
 		            <img src="images/portfolio/13- Anfitrionaje y Recepcion.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Anfitrionaje y Recepción</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-20.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-08.jpg">
 		            <img src="images/portfolio/20-Ingresos Espectaculares.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Ingresos Sorprendentes</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-15.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-16.jpg">
 		            <img src="images/portfolio/15- Show de Robots Leds.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Robot Led</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-17.jpg">
+		        <li>
 		            <img src="images/portfolio/17- Boveda del Recuerdo.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Boveda del Recuerdo</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-16.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-12.jpg">
 		            <img src="images/portfolio/16-Filmacion Profesional.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Filmación Profesional</span>
@@ -113,37 +113,37 @@
 		                <span class="header">Fotografía Profesional</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-22.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-21.jpg">
 		            <img src="images/portfolio/22-Video Reel.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Video Reel</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-19.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-17.jpg">
 		            <img src="images/portfolio/19-Sesion Fotografica.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Sesion de Fotos</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-14.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-07.jpg">
 		            <img src="images/portfolio/14-Produccion de Protocolo.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Producción de Protocolo</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-21.jpg">
+		        <li>
 		            <img src="images/portfolio/21- Party Games.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Party Games</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-23.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-13.jpg">
 		            <img src="images/portfolio/23-Video Mapping.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Mapping</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-26.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-14.jpg">
 		            <img src="images/portfolio/26-Pantalla Gigante.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Pantalla Gigante</span>
@@ -155,7 +155,7 @@
 		                <span class="header">Yearbook</span>
 		            </div>
 		        </li>
-		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-25.jpg">
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-19.jpg">
 		            <img src="images/portfolio/25- Photo Book.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Photobook</span>
@@ -171,6 +171,42 @@
 		            <img src="images/portfolio/28-Party Bus.png" alt="" />
 		            <div class="portfolio-item-content">
 		                <span class="header">Party Bus</span>
+		            </div>
+		        </li>
+		        <li>
+		            <img src="images/portfolio/29-gracias personalizadas.png" alt="" />
+		            <div class="portfolio-item-content">
+		                <span class="header">Gracias Personalizadas</span>
+		            </div>
+		        </li>
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-26.jpg">
+		            <img src="images/portfolio/30-Zona Selfie.png" alt="" />
+		            <div class="portfolio-item-content">
+		                <span class="header">Zona Selfie</span>
+		            </div>
+		        </li>
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-25.jpg">
+		            <img src="images/portfolio/31-Mesa de fotos.png" alt="" />
+		            <div class="portfolio-item-content">
+		                <span class="header">Mesa de Fotos</span>
+		            </div>
+		        </li>
+		        <li>
+		            <img src="images/portfolio/32-Sandwich Stations.png" alt="servicios_web-03.jpg" />
+		            <div class="portfolio-item-content">
+		                <span class="header">Sandwich Stations</span>
+		            </div>
+		        </li>
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-23.jpg">
+		            <img src="images/portfolio/33-Side by Side.png" alt="" />
+		            <div class="portfolio-item-content">
+		                <span class="header">Side by Side</span>
+		            </div>
+		        </li>
+		        <li data-toggle="modal" data-target="#modal-service" data-link="servicios_web-22.jpg">
+		            <img src="images/portfolio/34-Video Maniefesto.jpg" alt="" />
+		            <div class="portfolio-item-content">
+		                <span class="header">Video Manifiesto</span>
 		            </div>
 		        </li>
 		    </ul>
