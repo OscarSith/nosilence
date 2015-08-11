f3a6e03a9843ac9d5c74f73dd73a9fa23ad3cbb7
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2015-08-10 23:31:59 -0500

    Evento, Titulo de 15 años cambiado

--- resources/views/eventos.blade.php
+++ resources/views/eventos.blade.php
@@ -3,247 +3,248 @@
 @section('content')
 <div class="section quincianero">
 </div>
-<section id="tline" class="page-wrapper gray">
-<div class="container">
-	<h1 class="title text-center"><img src="images/titulo_mis_15.png" alt="Titulo mis 15 años"></h1>
-	<p class="tagline text-center">
-		 Sabemos que tu fiesta de 15 no es solo una noche de fiesta, es la NOCHE en que<br>tus sueños se hacen realidad.
-	</p>
-	<div class="row">
-		<div class="col-md-12">
-			<ul class="tline-holder">
-				<!-- tline ITEM-->
-				<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
-				<div class="tline-item-content">
-					<div class="date-icon fa fa-check">
-					</div>
-					<div class="tline-item-txt text-right">
-						<!--<div class="meta">
-							 January 2015
+<section id="tline" class="page-wrapper gray" style="padding-top:0">
+	<div class="container">
+		<h1 class="title text-center"><img src="images/titulo_mis_15.png" alt="Titulo mis 15 años" id="my15years"></h1>
+		<p class="tagline text-center">
+			 Sabemos que tu fiesta de 15 no es solo una noche de fiesta, es la NOCHE en que<br>tus sueños se hacen realidad.
+		</p>
+		<div class="row">
+			<div class="col-md-12">
+				<ul class="tline-holder">
+					<!-- tline ITEM-->
+					<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
+					<div class="tline-item-content">
+						<div class="date-icon fa fa-check">
 						</div>
-						<h3>Love Fashion</h3>-->
-						<p>
-							 Dá la bienvenida a tus invitados con una comparsa llena de color y alegría, con personajes temáticos y anfitriones que los guiarán hasta sus ubicaciones, mientras ellos esperan disfrutarán de un entretenido show de percusión que les brindará una recepción diferente.
-						</p>
+						<div class="tline-item-txt text-right">
+							<!--<div class="meta">
+								 January 2015
+							</div>
+							<h3>Love Fashion</h3>-->
+							<p>
+								 Dá la bienvenida a tus invitados con una comparsa llena de color y alegría, con personajes temáticos y anfitriones que los guiarán hasta sus ubicaciones, mientras ellos esperan disfrutarán de un entretenido show de percusión que les brindará una recepción diferente.
+							</p>
+						</div>
 					</div>
-				</div>
-				</li>
-				<!-- /tline-->
-				<!-- tline-->
-				<li class="tline-item-right wow fadeInRight animated" style="visibility: visible; animation-name: fadeInRight; -webkit-animation-name: fadeInRight;">
-				<div class="tline-item-content">
-					<div class="date-icon fa fa-check">
+					</li>
+					<!-- /tline-->
+					<!-- tline-->
+					<li class="tline-item-right wow fadeInRight animated" style="visibility: visible; animation-name: fadeInRight; -webkit-animation-name: fadeInRight;">
+					<div class="tline-item-content">
+						<div class="date-icon fa fa-check">
+						</div>
+						<div class="tline-item-txt text-left">
+							<p>
+								 Cuando llegue la hora de tu ingreso no dejes nada al azar, todo estará preparado para una espectacular aparición: desde descender de un globo aerostático, un elegante carruaje a caballos o hasta una mágica entrada a cargo de un reconocido mago, tus ideas junto a nuestro equipo creativo desarrollaran el ingreso de tus sueños.
+							</p>
+						</div>
 					</div>
-					<div class="tline-item-txt text-left">
-						<p>
-							 Cuando llegue la hora de tu ingreso no dejes nada al azar, todo estará preparado para una espectacular aparición: desde descender de un globo aerostático, un elegante carruaje a caballos o hasta una mágica entrada a cargo de un reconocido mago, tus ideas junto a nuestro equipo creativo desarrollaran el ingreso de tus sueños.
-						</p>
+					</li>
+					<!-- /tline-->
+					<!-- tline-->
+					<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
+					<div class="tline-item-content">
+						<div class="date-icon fa fa-check">
+						</div>
+						<div class="tline-item-txt text-right">
+							<p>
+								 La decoración y arquitectura se basaran en la temática que elijas, los colores del tomorrowland o la calidez de elementos vintage pondrán el toque especial a tu fiesta, cuidando cada detalle para que te transporten al lugar que soñaste.
+							</p>
+						</div>
 					</div>
-				</div>
-				</li>
-				<!-- /tline-->
-				<!-- tline-->
-				<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
-				<div class="tline-item-content">
-					<div class="date-icon fa fa-check">
+					</li>
+					<!-- /tline-->
+					<!-- tline-->
+					<li class="tline-item-right wow fadeInRight animated" style="visibility: visible; animation-name: fadeInRight; -webkit-animation-name: fadeInRight;">
+					<div class="tline-item-content">
+						<div class="date-icon fa fa-check">
+						</div>
+						<div class="tline-item-txt text-left">
+							<p>
+								 La pista de baile estará a cargo de nuestro dj oficial, DJ TOWA, con un playlist que se acomodará a tus gustos personales y de tus invitados, de esta manera, nadie se quedará sentado en tu fiesta.
+							</p>
+						</div>
 					</div>
-					<div class="tline-item-txt text-right">
-						<p>
-							 La decoración y arquitectura se basaran en la temática que elijas, los colores del tomorrowland o la calidez de elementos vintage pondrán el toque especial a tu fiesta, cuidando cada detalle para que te transporten al lugar que soñaste.
-						</p>
+					</li>
+					<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
+					<div class="tline-item-content">
+						<div class="date-icon fa fa-check">
+						</div>
+						<div class="tline-item-txt text-right">
+							<p>
+								 Las luces y el sonido harán vibrar tus sentidos con un show espectacular de sincronización y color al ritmos de la música, te brindamos lo último en tecnología de las fiestas RAVES para tu noche de quince.
+							</p>
+						</div>
 					</div>
-				</div>
-				</li>
-				<!-- /tline-->
-				<!-- tline-->
-				<li class="tline-item-right wow fadeInRight animated" style="visibility: visible; animation-name: fadeInRight; -webkit-animation-name: fadeInRight;">
-				<div class="tline-item-content">
-					<div class="date-icon fa fa-check">
+					</li>
+					<li class="tline-item-right wow fadeInRight animated" style="visibility: visible; animation-name: fadeInRight; -webkit-animation-name: fadeInRight;">
+					<div class="tline-item-content">
+						<div class="date-icon fa fa-check">
+						</div>
+						<div class="tline-item-txt text-left">
+							<p>
+								Deliciosos bocaditos y aperitivos serán el preámbulo a una sofisticada cena a cargo de nuestros cheffs que junto a una atención profesional de mozos convertirán  la cena en un momento de placer y elegancia.
+							</p>
+						</div>
 					</div>
-					<div class="tline-item-txt text-left">
-						<p>
-							 La pista de baile estará a cargo de nuestro dj oficial, DJ TOWA, con un playlist que se acomodará a tus gustos personales y de tus invitados, de esta manera, nadie se quedará sentado en tu fiesta.
-						</p>
+					</li>
+					<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
+					<div class="tline-item-content">
+						<div class="date-icon fa fa-check">
+						</div>
+						<div class="tline-item-txt text-right">
+							<p>
+								Una responsable Barra Libre brindará bebidas a tus invitados toda la noche, desde Cuba Libre hasta Machu Pichu, pasando por mojitos y pisco sur, disfruta de los mejores y más delicioso tragos controlados responsablemente por nuestro staff de bartenders junto con nuestro equipo de supervisión y seguridad.
+							</p>
+						</div>
 					</div>
-				</div>
-				</li>
-				<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
-				<div class="tline-item-content">
-					<div class="date-icon fa fa-check">
+					</li>
+					<!-- /tline-->
+					<li class="tline-start">
+					<div class="tline-start-content">
+						<div class="tline-start-icon">
+						</div>
+						<div class="meta wow zoomIn animated" style="visibility: visible; animation-name: zoomIn; -webkit-animation-name: zoomIn;">
+							La atención personalizada d nuestro equipo de planeas y creativos harán realidad tu noche,<br>cuidando cada detalle y esmerándose por hacerte sentir a ti y a tu familia como un invitado más.
+						</div>
+						<div class="text-center">
+							<br>
+							<p>CATÁLOGO DE EVENTOS</p>
+							<br>
+							<a href="#" class="btn btn-default btn-system">DESCARGAR</a>
+						</div>
 					</div>
-					<div class="tline-item-txt text-right">
-						<p>
-							 Las luces y el sonido harán vibrar tus sentidos con un show espectacular de sincronización y color al ritmos de la música, te brindamos lo último en tecnología de las fiestas RAVES para tu noche de quince.
-						</p>
-					</div>
-				</div>
-				</li>
-				<li class="tline-item-right wow fadeInRight animated" style="visibility: visible; animation-name: fadeInRight; -webkit-animation-name: fadeInRight;">
-				<div class="tline-item-content">
-					<div class="date-icon fa fa-check">
-					</div>
-					<div class="tline-item-txt text-left">
-						<p>
-							Deliciosos bocaditos y aperitivos serán el preámbulo a una sofisticada cena a cargo de nuestros cheffs que junto a una atención profesional de mozos convertirán  la cena en un momento de placer y elegancia.
-						</p>
-					</div>
-				</div>
-				</li>
-				<li class="tline-item-left wow fadeInLeft animated" style="visibility: visible; animation-name: fadeInLeft; -webkit-animation-name: fadeInLeft;">
-				<div class="tline-item-content">
-					<div class="date-icon fa fa-check">
-					</div>
-					<div class="tline-item-txt text-right">
-						<p>
-							Una responsable Barra Libre brindará bebidas a tus invitados toda la noche, desde Cuba Libre hasta Machu Pichu, pasando por mojitos y pisco sur, disfruta de los mejores y más delicioso tragos controlados responsablemente por nuestro staff de bartenders junto con nuestro equipo de supervisión y seguridad.
-						</p>
-					</div>
-				</div>
-				</li>
-				<!-- /tline-->
-				<li class="tline-start">
-				<div class="tline-start-content">
-					<div class="tline-start-icon">
-					</div>
-					<div class="meta wow zoomIn animated" style="visibility: visible; animation-name: zoomIn; -webkit-animation-name: zoomIn;">
-						La atención personalizada d nuestro equipo de planeas y creativos harán realidad tu noche,<br>cuidando cada detalle y esmerándose por hacerte sentir a ti y a tu familia como un invitado más.
-					</div>
-					<div class="text-center">
-						<br>
-						<p>CATÁLOGO DE EVENTOS</p>
-						<br>
-						<a href="#" class="btn btn-default btn-system">DESCARGAR</a>
-					</div>
-				</div>
-				</li>
-			</ul>
+					</li>
+				</ul>
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-12">
-			<div class="recent-projects">
-				<h4 class="title green-nosilence"><span>Fiestas Temáticas</span></h4>
-				<div class="projects-carousel touch-carousel">
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/1TOMORROWLAND.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>TOMORROWLAND</span>
-							</div>
-						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/2FIESTA EN LAS VEGAS.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>FIESTA EN LAS VEGAS</span>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="recent-projects">
+					<h4 class="title green-nosilence"><span>Fiestas Temáticas</span></h4>
+					<div class="projects-carousel touch-carousel">
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/1TOMORROWLAND.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>TOMORROWLAND</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<a class="lightbox">
-									<div class="thumb-overlay"><i class="fa fa-arrors-alt"></i></div>
-									<img alt="" src="images/tematica/3FIESTA LUAU.png" />
-								</a>
-							</div>
-							<div class="portfolio-details">
-								<span>FIESTA LUAU</span>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/2FIESTA EN LAS VEGAS.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>FIESTA EN LAS VEGAS</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/4FIESTA HINDU.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>FIESTA HINDU</span>
-							</div>
-						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/5CIRCUS PARTY.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>CIRCUS PARTY</span>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<a class="lightbox">
+										<div class="thumb-overlay"><i class="fa fa-arrors-alt"></i></div>
+										<img alt="" src="images/tematica/3FIESTA LUAU.png" />
+									</a>
+								</div>
+								<div class="portfolio-details">
+									<span>FIESTA LUAU</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/6FIESTA DISCO.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>FIESTA DISCO</span>
-							</div>
-						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/7FIESTA VINTAGE.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>FIESTA VINTAGE</span>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/4FIESTA HINDU.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>FIESTA HINDU</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/8HOLLYWOOD.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>HOLLYWOOD</span>
-							</div>
-						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/9PARTY IN THE SKY.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>PARTY IN THE SKY</span>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/5CIRCUS PARTY.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>CIRCUS PARTY</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/10GLOW PARTY.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>GLOW PARTY</span>
-							</div>
-						</div>
-					</div>
-                    <div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/11FIESTA EN PARIS.png" />
-							</div>
-							<div class="portfolio-details">
-								<span>FIESTA EN PARIS</span>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/6FIESTA DISCO.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>FIESTA DISCO</span>
+								</div>
 							</div>
 						</div>
-					</div>
-                    <div class="portfolio-item item">
-						<div class="portfolio-border">
-							<div class="portfolio-thumb">
-								<img alt="" src="images/tematica/12FIESTA PRIMAVERAL.png" />
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/7FIESTA VINTAGE.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>FIESTA VINTAGE</span>
+								</div>
 							</div>
-							<div class="portfolio-details">
-								<span>FIESTA PRIMAVERAL</span>
+						</div>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/8HOLLYWOOD.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>HOLLYWOOD</span>
+								</div>
+							</div>
+						</div>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/9PARTY IN THE SKY.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>PARTY IN THE SKY</span>
+								</div>
+							</div>
+						</div>
+						<div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/10GLOW PARTY.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>GLOW PARTY</span>
+								</div>
+							</div>
+						</div>
+	                    <div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/11FIESTA EN PARIS.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>FIESTA EN PARIS</span>
+								</div>
+							</div>
+						</div>
+	                    <div class="portfolio-item item">
+							<div class="portfolio-border">
+								<div class="portfolio-thumb">
+									<img alt="" src="images/tematica/12FIESTA PRIMAVERAL.png" />
+								</div>
+								<div class="portfolio-details">
+									<span>FIESTA PRIMAVERAL</span>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -251,6 +252,5 @@
 			</div>
 		</div>
 	</div>
-</div>
 </section>
 @stop
